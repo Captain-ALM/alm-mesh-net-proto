a421d2d1225bc68dada707393c608394f41255fa
Captain ALM <captainalm@captainalm.com>
2025-04-01 00:09:33 +0100

    Add support for IP packet encapsulation.

---
+++ src/com/captainalm/lib/mesh/packets/data/DataAddressedPayload.java
@@ -0,0 +1,69 @@
+package com.captainalm.lib.mesh.packets.data;
+
+import com.captainalm.lib.mesh.packets.Packet;
+
+/**
+ * Provides addressed IP packet storage for 4/6.
+ *
+ * @author Alfred Manville
+ */
+public class DataAddressedPayload extends DataPayload {
+    protected boolean treatAddressAsSender;
+
+    /**
+     * Constructs a new instance of DataAddressedPayload from a {@link Packet}.
+     *
+     * @param packet The packet to construct from.
+     * @param isSender Treat the tag address as sender.
+     */
+    public DataAddressedPayload(Packet packet, boolean isSender) {
+        super(packet);
+        treatAddressAsSender = isSender;
+    }
+
+    /**
+     * Constructs a new instance of DataAddressedPayload from an IP Packet.
+     *
+     * @param ipPacket The IP packet to encapsulate (4/6)
+     * @param isSender Treat the tag address as sender.
+     */
+    public DataAddressedPayload(byte[] ipPacket, boolean isSender) {
+        super(ipPacket);
+        treatAddressAsSender = isSender;
+    }
+
+    @Override
+    protected int actualDataLength() {
+        return super.actualDataLength() - 16;
+    }
+
+    /**
+     * Gets the IP packet, given the node ID addresses.
+     * The Node ID is ignored where the tagged address gets set to.
+     *
+     * @param sourceNodeID The source node ID.
+     * @param destinationNodeID The destination node ID.
+     * @return The encapsulated IP packet bytes.
+     */
+    @Override
+    public byte[] getIpPacket(byte[] sourceNodeID, byte[] destinationNodeID) {
+        if (dataSize < 16)
+            return ipPk;
+        if (super.getIpPacket(sourceNodeID, destinationNodeID) != null) {
+            if (treatAddressAsSender) {
+                if (ver == 4) {
+                    System.arraycopy(data, dataSize - 16 + dataStartIndex, ipPk, 12, 4);
+                } else if (ver == 6) {
+                    System.arraycopy(data, dataSize - 16 + dataStartIndex, ipPk, 8, 16);
+                }
+            } else {
+                if (ver == 4) {
+                    System.arraycopy(data, dataSize - 16 + dataStartIndex, ipPk, 16, 4);
+                } else if (ver == 6) {
+                    System.arraycopy(data, dataSize - 16 + dataStartIndex, ipPk, 24, 16);
+                }
+            }
+        }
+        return ipPk;
+    }
+}
---
+++ src/com/captainalm/lib/mesh/packets/data/DataPayload.java
@@ -0,0 +1,79 @@
+package com.captainalm.lib.mesh.packets.data;
+
+import com.captainalm.lib.mesh.packets.Packet;
+
+/**
+ * Provides IP packet storage for 4/6.
+ *
+ * @author Alfred Manville
+ */
+public class DataPayload extends PacketData {
+    protected byte[] ipPk;
+    protected int ver;
+
+    /**
+     * Constructs a new instance of DataPayload from a {@link Packet}.
+     *
+     * @param packet The packet to construct from.
+     */
+    public DataPayload(Packet packet) {
+        super(packet);
+    }
+
+    /**
+     * Constructs a new instance of DataPayload from an IP Packet.
+     *
+     * @param ipPacket The IP packet to encapsulate (4/6)
+     */
+    public DataPayload(byte[] ipPacket) {
+        super((ipPacket ==  null || ipPacket.length < 1) ? 0 : ((ipPacket[0] >> 4) == 4) ? ipPacket.length - 8 : (((ipPacket[0] >> 4) == 6) ? ipPacket.length - 32 : 0));
+        if (ipPacket != null && ipPacket.length > 0) {
+            ver = ipPacket[0] >> 4;
+            if (ver == 4) {
+                System.arraycopy(ipPacket, 0, data, 0, 12);
+                System.arraycopy(ipPacket, 20, data, 12, ipPacket.length - 20);
+            } else if (ver == 6) {
+                System.arraycopy(ipPacket, 0, data, 0, 8);
+                System.arraycopy(ipPacket, 40, data, 8, ipPacket.length - 40);
+            }
+            ipPk = ipPacket;
+        }
+    }
+
+    protected int actualDataLength() {
+        return dataSize;
+    }
+
+    /**
+     * Gets the IP packet, given the node ID addresses.
+     *
+     * @param sourceNodeID The source node ID.
+     * @param destinationNodeID The destination node ID.
+     * @return The encapsulated IP packet bytes.
+     */
+    public byte[] getIpPacket(byte[] sourceNodeID, byte[] destinationNodeID) {
+        if (ipPk == null && actualDataLength() > 0 && sourceNodeID != null && sourceNodeID.length > 16 && destinationNodeID != null && destinationNodeID.length > 16) {
+            ver = data[0] >> 4;
+            if (ver == 4) {
+                ipPk = new byte[actualDataLength() + 8];
+                System.arraycopy(data, dataStartIndex, ipPk, 0, 12);
+                ipPk[12] = 10; //10.x.x.x
+                System.arraycopy(sourceNodeID, 0, ipPk, 13, 3);
+                ipPk[16] = 10; //10.x.x.x
+                System.arraycopy(destinationNodeID, 0, ipPk, 17, 4);
+                System.arraycopy(data, dataStartIndex + 12, ipPk, 20, actualDataLength() - 12);
+            } else if (ver == 6) {
+                ipPk = new byte[actualDataLength() + 32];
+                System.arraycopy(data, dataStartIndex, ipPk, 0, 8);
+                ipPk[8] = (byte) 253; //fd0a::
+                ipPk[9] = 10;
+                System.arraycopy(sourceNodeID, sourceNodeID.length - 14, ipPk, 10, 14);
+                ipPk[24] = (byte) 253; //fd0a::
+                ipPk[25] = 10;
+                System.arraycopy(destinationNodeID, destinationNodeID.length - 14, ipPk, 26, 14);
+                System.arraycopy(data, dataStartIndex + 8, ipPk, 40, actualDataLength() - 8);
+            }
+        }
+        return ipPk;
+    }
+}
